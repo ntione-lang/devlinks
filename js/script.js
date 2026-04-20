b4d048b6b5df1303c19f3c2c0e2b61f231f8dc33
ntione-lang <ntione@gmail.com>
2026-04-20 09:42:56 -0300

    Atualização

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  //pegar a tag imag
+  const img = document.querySelector("#profile img");
+
+  // substituir a img
+  if (html.classList.contains("light")) {
+    // se tiver no modo light, adicionar img light
+    img.setAttribute("src", "/assets/avatar-light.png");
+  } else {
+    // se tiver light mode, adicione a imagem dark
+    img.setAttribute("src", "/assets/avatar.png");
+  }
+}
